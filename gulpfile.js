e967bf63e39f9979bb260b8746a5e6208cd98e2b
Sai <clapgames.elearn@gmail.in>
2017-01-16 20:08:47 +0530

    Improvised gulp task runners

--- gulpfile.js
+++ gulpfile.js
@@ -16,12 +16,18 @@ var banner = ['/*!\n',
     ''
 ].join('');
 
+var appConfig  = {
+    base : './app/',
+    dest : './dest/',
+    tmp : './tmp/'
+};
+
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
-    return gulp.src('less/clapgames.less')
+    return gulp.src( appConfig.base + 'less/**/*.less' )
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest(appConfig.tmp + 'css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -29,10 +35,10 @@ gulp.task('less', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/clapgames.css')
+    return gulp.src(appConfig.tmp + 'css/**/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
+        .pipe(gulp.dest(appConfig.dest + 'css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -40,11 +46,11 @@ gulp.task('minify-css', ['less'], function() {
 
 // Minify JS
 gulp.task('minify-js', function() {
-    return gulp.src('js/clapgames.js')
+    return gulp.src(appConfig.base + 'js/clapgames.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest(appConfig.dest + 'js'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -53,10 +59,16 @@ gulp.task('minify-js', function() {
 // Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
-
+        .pipe(gulp.dest(appConfig.dest + 'vendor/bootstrap'))
+ 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        .pipe(gulp.dest(appConfig.dest + 'vendor/jquery'))
+
+    gulp.src([appConfig.base + 'img/**/*'])
+        .pipe(gulp.dest(appConfig.dest + 'img/'))
+   
+    gulp.src([appConfig.base + '**/*.html'])
+        .pipe(gulp.dest(appConfig.dest))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -66,7 +78,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest(appConfig.dest + 'vendor/font-awesome'))
 })
 
 // Run everything
@@ -76,17 +88,12 @@ gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
 gulp.task('browserSync', function() {
     browserSync.init({
         server: {
-            baseDir: ''
+            baseDir: appConfig.dest
         },
     })
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js', 'copy'], function() {
+    gulp.watch(appConfig.base + '**/*.*', ['less', 'minify-css', 'minify-js', 'copy', browserSync.reload]);
 });
